21d9ce6d900eba1e3e2366daa8c6b285d77e1040
Benjamin Sherman <benjamin@bensherman.io>
2019-07-04 11:59:19 -0400

    Search functions need content

--- bot/triggers/commands/lmdtfy.py
+++ bot/triggers/commands/lmdtfy.py
@@ -6,7 +6,7 @@ from urllib.parse import urlencode
 class Lmdtfy(Command):
     names = ["lmdtfy", "search"]
     description = "Helps someone look something up on the superior search engine"
-    needsContent = False
+    needsContent = True
 
     async def execute_command(self, client, msg, content):
         url = "https://lmgtfy.com/?s=d&" + urlencode({"q": content})
@@ -16,7 +16,7 @@ class Lmdtfy(Command):
 class Lmgtfy(Command):
     names = ["lmgtfy"]
     description = "Helps someone look something up on the inferior search engine"
-    needsContent = False
+    needsContent = True
 
     async def execute_command(self, client, msg, content):
         url = "https://lmgtfy.com/?" + urlencode({"q": content})
