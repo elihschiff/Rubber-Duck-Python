8bcfb5e41474322aa70fe86a9a9eefe98db26931
Benjamin Sherman <benjamin@bensherman.io>
2019-07-05 00:58:51 -0400

    Restructure bot token parsing

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ import os
 
 duck = DuckClient()
 
-duck.run(os.environ["BOT_TOKEN"])
+bot_token = ""
+with open("bot_token", "r") as token_file:
+    bot_token = token_file.readline().strip()
+
+duck.run(bot_token)
