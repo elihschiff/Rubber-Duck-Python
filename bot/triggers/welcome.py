f72e8b592b792831b181af6c562f67ec70d25a37
Eli Schiff <elihschiff@gmail.com>
2019-07-05 21:39:46 -0400

    added comment

--- bot/triggers/welcome.py
+++ bot/triggers/welcome.py
@@ -7,3 +7,5 @@ class Welcome(NewMemberTriggers):
         welcomeChannel = member.guild.get_channel(client.config["welcome_channel_id"])
         if welcomeChannel:
             await welcomeChannel.send(client.messages["welcome"].format(member.mention))
+
+    # TODO this only sends the public part, the dm still needs to be added
