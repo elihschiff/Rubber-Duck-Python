217013230b5a28804955a0c53d1fd3c731f2f593
Eli Schiff <elihschiff@gmail.com>
2019-07-05 22:16:14 -0400

    undid the bug I made

--- bot/triggers/welcome.py
+++ bot/triggers/welcome.py
@@ -6,9 +6,6 @@ class Welcome(NewMemberTriggers):
     async def execute(self, client, member):
         welcomeChannel = member.guild.get_channel(client.config["welcome_channel_id"])
         if welcomeChannel:
-
-
-
             await welcomeChannel.send(client.messages["welcome"].format(member.mention))
 
     # TODO this only sends the public part, the dm still needs to be added
