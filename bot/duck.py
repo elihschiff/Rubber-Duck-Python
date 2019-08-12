9afd382d04870c53efd239ffa420ea15d73e9813
Benjamin Sherman <benjamin@bensherman.io>
2019-08-12 13:56:26 -0400

    Add basic logging support

--- bot/duck.py
+++ bot/duck.py
@@ -9,11 +9,14 @@ from .triggers import new_member_triggers
 from .triggers.quack import quack
 from .triggers.emoji_mode import invalid_emoji_message
 
+from . import logging
+
 
 class DuckClient(discord.Client):
     def __init__(
         self,
         config_filename="config/config.json",
+        logging_filename="config/logging.json",
         messages_filename="config/messages.json",
         quacks_filename="config/quacks.txt",
     ):
@@ -23,8 +26,14 @@ class DuckClient(discord.Client):
             config_filename
         )  # TODO: remove this once emoji mode uses the database
 
+        self.logging_filename = (
+            logging_filename
+        )  # TODO: remove this once logging uses the database
+
         with open(config_filename, "r") as config_file:
             self.config = json.load(config_file)
+        with open(logging_filename, "r") as logging_file:
+            self.logging = json.load(logging_file)
         with open(messages_filename, "r") as messages_file:
             self.messages = json.load(messages_file)
         with open(quacks_filename, "r") as quacks_file:
@@ -38,6 +47,8 @@ class DuckClient(discord.Client):
         print(f"Connected as {self.user}!")
 
     async def on_message(self, msg):
+        await logging.log(self, msg)
+
         if msg.author.bot:
             return
 
---
+++ bot/logging.py
@@ -0,0 +1,69 @@
+from discord import DMChannel, File
+
+
+async def log(client, msg):
+    LOG_SERVER = client.get_guild(client.logging["LOG_SERVER_ID"])
+    if LOG_SERVER is None:
+        # check does not need to be here (log_server is only necessary in the case
+        # that no corresponding logging channel exists) since the client can receive
+        # a channel by its id alone, but putting the check here helps ensuring a sane
+        # config file
+        return
+
+    json_dirty = False
+
+    destination_channel = None
+    log_content = None
+
+    attached_files = []
+    for attachment in msg.attachments:
+        saved_attachment = File(f"/tmp/{msg.id}-{attachment.filename}")
+        await attachment.save(saved_attachment.fp, use_cached=True)
+        attached_files.append(saved_attachment)
+
+    if msg.channel is DMChannel:
+        destination_channel = LOG_SERVER.get_channel(client.logging["DM_LOGS"])
+        rcvd_channel_tag = ""
+        if msg.author == client.user:
+            rcvd_channel_tag = f" from {msg.author.name} ({msg.author.id})"
+
+        log_content = f"{msg.author.name} ({msg.author.id}){rcvd_channel_tag}: {msg.clean_content}"
+    else:
+        if msg.channel.id not in client.logging:
+            source_category = msg.channel.category
+            destination_category = None
+            if source_category.id not in client.logging:
+                destination_category = await LOG_SERVER.create_category_channel(
+                    source_category.name
+                )
+                client.logging[
+                    f"{source_category.id}"
+                ] = destination_category.id
+            else:
+                destination_category = LOG_SERVER.get_channel(
+                    client.logging(f"{source_category.id}")
+                )
+
+            destination_channel = await LOG_SERVER.create_text_channel(
+                msg.channel.name, category=destination_category
+            )
+            client.logging[f"{msg.channel.id}"] = destination_channel.id
+
+            json_dirty = True
+        else:
+            destination_channel = LOG_SERVER.get_channel(
+                client.logging[f"{msg.channel.id}"]
+            )
+
+        log_content = (
+            f"{msg.author.name} ({msg.author.id}): {msg.clean_content}"
+        )
+
+    await destination_channel.send(log_content, files=attached_files)
+
+    for attached_file in attached_files:
+        os.remove(attached_file.filename)
+
+    if json_dirty:
+        with open(client.logging_filename, "w") as logging_file:
+            json.dump(client.logging, logging_file)
