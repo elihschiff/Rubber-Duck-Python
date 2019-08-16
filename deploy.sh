a19378a27cfaf54ad4e1324d507e96a002c17904
Benjamin Sherman <benjamin@bensherman.io>
2019-08-16 14:18:45 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@
 git fetch --all
 git reset --hard origin/master
 
+cp config/messages.json.example config/messages.json
+cp config/quacks.txt.example config/quacks.txt
+
 pip3 install -r requirements.txt
 
 nohup python3 main.py $(pidof python3) > /dev/null 2> /dev/null < /dev/null &
