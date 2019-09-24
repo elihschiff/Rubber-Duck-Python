b1454137e049e0697b44c72f1581913ca8ae30da
Eli Schiff <eli@elischiff.org>
2019-09-24 14:19:26 -0400

    fixed !help

--- bot/triggers/commands/help.py
+++ bot/triggers/commands/help.py
@@ -9,11 +9,31 @@ class Help(Command):
     needsContent = False
 
     async def execute_command(self, client, msg, content):
-        response = discord.Embed()
-        commands_str = ""
+        commands_arr = []
         for command in all_commands:
-            commands_str += (
-                f"**{command.prefixes[0]}{command.names[0]}:** {command.description}\n"
-            )
-        response.add_field(name="General Commands", value=commands_str, inline=True)
-        await utils.delay_send(msg.channel, "", embed=response)
+            if command.description:
+                commands_arr.append(
+                    f"**{command.prefixes[0]}{command.names[0]}:** {command.description}\n"
+                )
+
+        max_char_count = 2000
+        commands_str = ""
+        title = "General Commands"
+        for line in commands_arr:
+            if len(commands_str) + len(line) < max_char_count:
+                commands_str += line
+            else:
+                response = discord.Embed(title=title, description=commands_str)
+                await send_embed(msg, response, title)
+                commands_str = line
+                title = ""
+
+        response = discord.Embed(title=title, description=commands_str)
+        await send_embed(msg, response, title)
+
+
+async def send_embed(msg, embed, title):
+    if title:
+        await utils.delay_send(msg.channel, "", embed=embed)
+    else:
+        await msg.channel.send("", embed=embed)
