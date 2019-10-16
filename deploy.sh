0037cb981b70b785ec8949f83218918c0bf845a0
Benjamin Sherman <benjamin@bensherman.io>
2019-10-16 14:24:30 -0400

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-git fetch --all
-git reset --hard origin/master
-
 cp config/messages.json.example config/messages.json
 cp config/quacks.txt.example config/quacks.txt
 cp config/games.txt.example config/games.txt
