8a63ee7dc6bc246ceadd1fd988e99692b5aeb8ec
Benjamin Sherman <benjamin@bensherman.io>
2019-11-14 12:07:05 -0500

    Fix error on invalid response from Wolfram Alpha

--- bot/triggers/commands/math.py
+++ bot/triggers/commands/math.py
@@ -15,20 +15,23 @@ class Math(Command):
     async def execute_command(self, client, msg, content):
         wolfram = wolframalpha.Client(client.config["WOLFRAM_ID"])
         query_res = wolfram.query(content)
-        for pod in query_res.pods:
-            if (
-                pod.title.startswith("Result")
-                or pod.title.startswith("Exact result")
-                or pod.title.startswith("Power of 10 representation")
-                or pod.title.startswith("Decimal approximation")
-            ):
-                await msg.channel.send(f"The answer for `{content}` is: `{pod.text}`")
-                return
+        try:
+          for pod in query_res.pods:
+              if (
+                  pod.title.startswith("Result")
+                  or pod.title.startswith("Exact result")
+                  or pod.title.startswith("Power of 10 representation")
+                  or pod.title.startswith("Decimal approximation")
+              ):
+                  await msg.channel.send(f"The answer for `{content}` is: `{pod.text}`")
+                  return
 
-            if pod.title.startswith("Plot"):
-                await msg.channel.send(
-                    # TODO: this should be more robust
-                    f"The answer for `{content}` is: {list(list(pod.subpod)[0].img)[0]['@src']}"
-                )
-                return
+              if pod.title.startswith("Plot"):
+                  await msg.channel.send(
+                      # TODO: this should be more robust
+                      f"The answer for `{content}` is: {list(list(pod.subpod)[0].img)[0]['@src']}"
+                  )
+                  return
+        except KeyError:
+          pass
         await msg.channel.send(f"I could not find an answer for `{content}`")
