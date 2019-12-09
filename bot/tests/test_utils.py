97a1713327e1e8e510d556c5955d0f6c51bc4025
Benjamin Sherman <benjamin@bensherman.io>
2019-12-09 13:04:18 -0500

    Stop using deprecated asyncio coroutines
    asyncio.coroutine() was deprecated in Python 3.8 and is scheduled to be
    removed in Python 3.10.  This moves us to use the accepted syntax.

--- bot/tests/test_utils.py
+++ bot/tests/test_utils.py
@@ -2,9 +2,8 @@ import asyncio
 import shutil
 
 
-def async_test(f):
+def async_test(coro):
     def wrapper(*args, **kwargs):
-        coro = asyncio.coroutine(f)
         future = coro(*args, **kwargs)
         loop = asyncio.get_event_loop()
         loop.run_until_complete(future)
