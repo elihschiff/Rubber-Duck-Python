07c7d1842cee810c7f3c0454f13000c7d102ee4f
Benjamin Sherman <benjamin@bensherman.io>
2019-12-11 16:45:03 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
 from bot.duck import *
 from dotenv import load_dotenv
 import os
-import sqlite3
-import sys
-
-connection = sqlite3.connect(sys.path[0] + "/" + "database.db")
-c = connection.cursor()
-
-c.execute("SELECT * FROM classes")
-records = c.fetchall()
-if len(records) >= 1:
-    print(len(records), "classes found in table")
-    # for i in records:
-    #     print(i)
-else:
-    print("No classes in table")
 
 # loads variables from local .env file as environment variables (use os.getenv)
 load_dotenv()
