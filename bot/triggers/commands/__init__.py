d810389a7a63f16d02c88eb8dbf01b2501747c53
Benjamin Sherman <benjamin@bensherman.io>
2020-04-17 21:56:59 -0400

    Disable !dining

--- bot/triggers/commands/__init__.py
+++ bot/triggers/commands/__init__.py
@@ -79,7 +79,6 @@ from .code import Code
 from .connectfour import ConnectFour
 from .cpp_ref import CppRef
 from .delete import Delete
-from .dining import Dining
 from .echo import Echo
 from .emoji_mode import EmojiMode
 from .java import Java
@@ -110,7 +109,6 @@ all_commands = [
     ConnectFour(),
     CppRef(),
     Delete(),
-    Dining(),
     Echo(),
     EmojiMode(),
     Java(),
