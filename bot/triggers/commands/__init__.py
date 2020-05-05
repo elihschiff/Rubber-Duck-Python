54136c3a80c656acddc843262d3c6306b7256427
Benjamin Sherman <benjamin@bensherman.io>
2020-05-05 19:56:45 -0400

    Fix import order

--- bot/triggers/commands/__init__.py
+++ bot/triggers/commands/__init__.py
@@ -7,6 +7,69 @@ from .. import utils
 from ..message_trigger import MessageTrigger
 
 
+# Import all command classes here
+from .ai import AI
+from .choice import Choice
+from .class_management import AddClass, RemoveClass
+from .code import Code
+from .connectfour import ConnectFour
+from .cpp_ref import CppRef
+from .delete import Delete
+from .echo import Echo
+from .emoji_mode import EmojiMode
+from .java import Java
+from .issue import Issue
+from .latex import Latex  # latex machine broke
+from .list_classes import ListClasses
+from .lmdtfy import Lmdtfy, Lmgtfy
+from .man import Man
+from .math import Math
+from .minesweeper import Minesweeper
+from .poll import Poll
+from .rand import Random
+from .rgb import RGB
+from .rps import RockPaperScissors
+from .steam import Steam
+from .tictactoe import TicTacToe
+from .translate import Translate
+from .version import Version
+from .wikipedia import Wikipedia
+from .xkcd import Xkcd
+
+# NB: Please keep this in alphabetical order to maintain organized code
+ALL_COMMANDS = [
+    AddClass(),
+    AI(),
+    Choice(),
+    Code(),
+    ConnectFour(),
+    CppRef(),
+    Delete(),
+    Echo(),
+    EmojiMode(),
+    Java(),
+    Issue(),
+    Latex(),
+    ListClasses(),
+    Lmdtfy(),
+    Lmgtfy(),
+    Man(),
+    Math(),
+    Minesweeper(),
+    Poll(),
+    Random(),
+    RemoveClass(),
+    RGB(),
+    RockPaperScissors(),
+    Steam(),
+    TicTacToe(),
+    Translate(),
+    Version(),
+    Wikipedia(),
+    Xkcd(),
+]
+
+
 class Command(MessageTrigger):
     prefixes = ["!"]
     requires_mod = False
@@ -80,69 +143,6 @@ class Command(MessageTrigger):
 
     def __lt__(self, other):
         return self.names[0] < other.names[0]
-
-
-from .ai import AI
-from .choice import Choice
-from .class_management import AddClass, RemoveClass
-from .code import Code
-from .connectfour import ConnectFour
-from .cpp_ref import CppRef
-from .delete import Delete
-from .echo import Echo
-from .emoji_mode import EmojiMode
-from .java import Java
-from .issue import Issue
-from .latex import Latex  # latex machine broke
-from .list_classes import ListClasses
-from .lmdtfy import Lmdtfy, Lmgtfy
-from .man import Man
-from .math import Math
-from .minesweeper import Minesweeper
-from .poll import Poll
-from .rand import Random
-from .rgb import RGB
-from .rps import RockPaperScissors
-from .steam import Steam
-from .tictactoe import TicTacToe
-from .translate import Translate
-from .version import Version
-from .wikipedia import Wikipedia
-from .xkcd import Xkcd
-
-# Commands will auto alphabetize
-ALL_COMMANDS = [
-    AddClass(),
-    AI(),
-    Choice(),
-    Code(),
-    ConnectFour(),
-    CppRef(),
-    Delete(),
-    Echo(),
-    EmojiMode(),
-    Java(),
-    Issue(),
-    Latex(),
-    ListClasses(),
-    Lmdtfy(),
-    Lmgtfy(),
-    Man(),
-    Math(),
-    Minesweeper(),
-    Poll(),
-    Random(),
-    RemoveClass(),
-    RGB(),
-    RockPaperScissors(),
-    Steam(),
-    TicTacToe(),
-    Translate(),
-    Version(),
-    Wikipedia(),
-    Xkcd(),
-]
-ALL_COMMANDS.sort()
 
 
 async def invalid_command(client, msg):
