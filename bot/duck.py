fcacd8eed74e88ea6a6088553200e4633421254e
Benjamin Sherman <benjamin@bensherman.io>
2020-05-18 12:06:17 -0400

    Remove on message edits as well

--- bot/duck.py
+++ bot/duck.py
@@ -143,6 +143,21 @@ class DuckClient(discord.Client):
 
         if user.bot:
             return
+
+        try:
+            if (
+                "discord.gg/" in msg_full.content
+                or "discordapp.com/invite/" in msg_full.content
+                or "discord.com/invite/" in msg_full.content
+            ):
+                await msg.delete()
+                await msg.author.send(
+                    "Your message (`{utils.sanitized(msg.content)}`) has been removed because it contained a discord server invite link."
+                )
+                return
+        except:
+            await utils.sendTraceback(self, msg.content)
+
         try:
             await logging.log_message(self, msg_full, "(EDITED)")
             await invalid_emoji_message(self, msg_full)
