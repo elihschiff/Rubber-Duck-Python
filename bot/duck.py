bacc86f61ed45ded6630975c66799ac220333b87
Benjamin Sherman <benjamin@bensherman.io>
2020-05-18 09:48:34 -0400

    Automatically remove messages containing discord invites

--- bot/duck.py
+++ bot/duck.py
@@ -97,6 +97,17 @@ class DuckClient(discord.Client):
         if await invalid_emoji_message(self, msg):
             return
 
+        if (
+            "discord.gg/" in msg.content
+            or "discordapp.com/invite/" in msg.content
+            or "discord.com/invite/" in msg.content
+        ):
+            await msg.delete()
+            await msg.author.send(
+                "Your message (`{utils.sanitized(msg.content)}`) has been removed because it contained a discord server invite link."
+            )
+            return
+
         replied = False
         best_trigger = None
         best_trigger_idx = 0
