766799a51087abef308e3bbb53c75f351480c448
Benjamin Sherman <benjamin@bensherman.io>
2020-12-20 23:57:56 -0500

    Revert "Hopefully make math command more robust"
    This reverts commit 729108190e3365ed42b66b76494f48c0065f44d5.

--- bot/triggers/commands/math.py
+++ bot/triggers/commands/math.py
@@ -7,7 +7,7 @@ class Math(Command):
     names = ["math", "calc", "calculate", "solve"]
     description = "Solves a math problem"
     usage = "!math [expression]"
-    examples = f"!math d/dx sin(x)^2"
+    examples = "!math d/dx sin(x)^2"
 
     async def execute_command(self, client, msg, content, **kwargs):
         if not content:
@@ -15,19 +15,27 @@ class Math(Command):
             return
 
         wolfram = wolframalpha.Client(client.config["wolfram_id"])
-        res = wolfram.query(content)
+        query_res = wolfram.query(content)
         try:
-            result = next(res.results)
-
-            if result.title.startswith("Plot"):
-                await msg.channel.send(
-                    f"The answer for `{content}` is: {result.subpod.img['@src']}"
-                )
-            else:
-                await msg.channel.send(
-                    f"The answer for `{content}` is: `{result.plainText}`"
-                )
-        except (AttributeError, StopIteration):
-            await msg.channel.send(
-                f"I could not find an answer for `{utils.sanitized(content)}`"
-            )
+            for pod in query_res.pods:
+                if (
+                    pod.title.startswith("Result")
+                    or pod.title.startswith("Exact result")
+                    or pod.title.startswith("Power of 10 representation")
+                    or pod.title.startswith("Decimal approximation")
+                ):
+                    await msg.channel.send(
+                        f"The answer for `{content}` is: `{pod.text}`"
+                    )
+                    return
+                if pod.title.startswith("Plot"):
+                    await msg.channel.send(
+                        # TODO: this should be more robust
+                        f"The answer for `{content}` is: {list(list(pod.subpod)[0].img)[0]['@src']}"
+                    )
+                    return
+        except (KeyError, AttributeError):
+            pass
+        await msg.channel.send(
+            f"I could not find an answer for `{utils.sanitized(content)}`"
+        )
