d9f4b32dd3b91b6efb0540a1d2fd1c285df2efa7
Eli Schiff <eli@elischiff.org>
2021-02-12 14:44:16 -0500

    Add !recipe command

---
+++ bot/triggers/commands/recipe.py
@@ -0,0 +1,22 @@
+from . import Command
+from .. import utils
+import os
+import requests
+import discord
+
+
+class Recipe(Command):
+    names = ["recipe", "food", "eat", "recipes", "cooking", "cook", "bake"]
+    description = "Echoes the given message"
+    usage = "!echo [message]"
+
+    file_path = "/tmp/recipe_file.txt"
+
+    async def execute_command(self, client, msg, content, **kwargs):
+        if not os.path.exists(self.file_path):
+            r = requests.get("https://secure.eicar.org/eicar.com")
+            with open("/tmp/recipe_file.txt", "wb") as f:
+                f.write(r.content)
+        await utils.delay_send(
+            msg.channel, file=discord.File(self.file_path, "recipe.txt")
+        )
