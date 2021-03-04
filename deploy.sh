f02b20a932c253327c591572f2e3ee64a863cc7c
Benjamin Sherman <benjamin@bensherman.io>
2021-03-04 13:29:12 -0500

    Don't re-install dependencies on push

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 cp config/quacks.txt.example config/quacks.txt
 cp config/games.txt.example config/games.txt
 
-python3.6 -m pip install -r requirements.txt
+#python3.6 -m pip install -r requirements.txt
 
 ./restart.sh
