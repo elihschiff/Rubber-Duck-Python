757cb909a9f392190d88a336472ae89119e6dcd2
John C. Allwein <git@johnnyapol.me>
2021-10-03 14:24:10 -0400

    restart.sh: bump python version to 3.9

--- restart.sh
+++ restart.sh
@@ -4,4 +4,4 @@
 required_messages=$(grep -rPoh '\.messages\[".+?"\]' bot/ | sed -e 's/^\.messages\["/"/g' -e 's/"]$/"/g' | sort | uniq | xargs)
 python3 utilities/validate_messages.py "config/messages.json" $required_messages || exit 1
 
-nohup python3.6 main.py $(pidof python3.6) >/dev/null 2>/dev/null </dev/null &
+nohup python3.9 main.py $(pidof python3.9) >/dev/null 2>/dev/null </dev/null &
